c8bbbe0c16d119828aff4b0f13b52c8f98c50b32
NazarioRonaldo <aemir280511@gmail.com>
2023-05-22 15:57:55 +0300

    Минимальна рабочая версия

---
+++ Main.py
@@ -0,0 +1,19 @@
+from pygame import *
+
+back = (200, 255, 255)
+win_w = 600
+win_h = 500
+window = display.set_mode((win_w, win_h))
+window.fill(back)
+
+game = True
+finish = False
+clock = time.Clock()
+FPS = 60
+
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+    display.update()
+    clock.tick(FPS)
