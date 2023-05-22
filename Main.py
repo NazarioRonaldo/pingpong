2590c279aea8d65194056b5db049e2a71433e1c8
NazarioRonaldo <aemir280511@gmail.com>
2023-05-22 16:05:42 +0300

    Добавили класс GameSprite

--- Main.py
+++ Main.py
@@ -6,6 +6,20 @@ win_h = 500
 window = display.set_mode((win_w, win_h))
 window.fill(back)
 
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed, w, h) -> None:
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (w, h))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+
+        
 game = True
 finish = False
 clock = time.Clock()
